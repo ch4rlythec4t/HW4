cb9d51d973ac01cc56b2adecf7d05d9b5087f5b7
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-17 16:43:28 +0300

    Task25 done

---
+++ Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static int PowNatural(int Num, int Pow)
+    {
+        int result = 1;
+        for (int i = 1; i <= Pow; i++)
+        {
+            result = result * Num;
+        }
+        return result;
+    }
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите натуральную степень: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        int Result = PowNatural(Num: A, Pow: B);
+        Console.WriteLine($"Результат: {Result}");
+    }
+}
