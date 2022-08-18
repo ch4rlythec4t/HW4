4349036fbc5ce1b2f2f90cc6520f221528674b92
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-18 11:50:42 +0300

    Task29_Option2 done

--- Task29(option2)/Program.cs
+++ Task29(option2)/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static int[] CreateArray(int n)
+    {
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите  Array[{i}]: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return array;
+    }
+
+    static void WriteArray(int[] array, int arrayLength)
+    {
+        for (int i = 0; i < arrayLength; i++)
+        {
+            if (i == arrayLength - 1)
+            {
+                Console.Write(array[i] + "]");
+            }
+            else if (i == 0)
+            {
+                Console.Write("[" + array[i] + ", ");
+            }
+            else
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+    }
+    static void Main()
+    {
+        Console.Write("Введите N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        WriteArray(CreateArray(N), N);
+    }
+}
